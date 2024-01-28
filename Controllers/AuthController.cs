92b8fece8b8221ea805769576aa2af1201b49cef
Nisreen Wardeh <Nisreen.Wardeh@techprocess.net>
2024-01-28 16:30:28 +0300

    Commit1

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -23,34 +23,6 @@ namespace AuthenticationAutherization.Controllers
         public async Task<IActionResult> Login([FromBody] LoginModel model)
         {
             var result = await _authService.Login(model);
-            // Assuming you have the token as a string
-            string token = result.Token;
-
-            // Create a HttpClient for sending the request
-            HttpClient client = new HttpClient();
-
-            // Add the token to the Authorization header
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            // The URL of the target microservice
-            string url = "https://localhost:7035/api/Courses/x";
-
-            // Send the request
-            HttpResponseMessage response = await client.GetAsync(url);
-
-            // Check the response
-            if (response.IsSuccessStatusCode)
-            {
-                // The request was successful
-                string responseContent = await response.Content.ReadAsStringAsync();
-            }
-            else
-            {
-                // The request failed
-            }
-            //HttpClient client = new HttpClient();
-            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.Token);
-            //HttpResponseMessage response = await client.GetAsync("https://localhost:7048");
             return Ok(new { token = result.Token });
         }
         [HttpPost("Register")]
--- Program.cs
+++ Program.cs
@@ -28,23 +28,45 @@ builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(o =>
 {
-   // o.RequireHttpsMetadata = false;
-    o.SaveToken = true;
+    // o.RequireHttpsMetadata = false;
+    o.SaveToken = false;
 
     o.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuerSigningKey = true,
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        //ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        //ValidAudience = builder.Configuration["Jwt:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey
         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
     };
 });
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//}).AddJwtBearer(o =>
+//{
+//   // o.RequireHttpsMetadata = false;
+//    o.SaveToken = true;
+
+//    o.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuerSigningKey = true,
+//        ValidateIssuer = true,
+//        ValidateAudience = true,
+//        ValidateLifetime = true,
+//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//        ValidAudience = builder.Configuration["Jwt:Audience"],
+//        IssuerSigningKey = new SymmetricSecurityKey
+//        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+//    };
+//});
 
 
 builder.Services.AddControllers();
